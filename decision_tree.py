9e62b1a8956c01be71e25543f92d27d4f225d052
Kripash Shrestha <kripashs@gmail.com>
2019-09-15 17:06:35 -0700

    Add files via upload
    dictionary for tracking which features have been used

--- decision_tree.py
+++ decision_tree.py
@@ -1,24 +1,27 @@
 import numpy as np
 import math
+import copy
 
 X = np.array([[0,1,0,1],[1,1,1,1],[0,0,0,1]])
 Y = np.array([[1], [1], [0]])
 max_depth = 2
 
+feature_dict = {}
+
 class Node():
   def __init__(self, value, node_left, node_right):
-    self.node_left = node_left
-    self.node_right = node_right
-    self.value = value
+    self.node_left = copy.copy(node_left)
+    self.node_right = copy.copy(node_right)
+    self.value = copy.copy(value)
 
   def set_left(self, left):
-    self.node_left = left
+    self.node_left = copy.copy(left)
 
   def set_right(self, right):
-    self.node_right = right
+    self.node_right = copy.copy(right)
 
   def set_value(self, value):
-    self.value = value
+    self.value = copy.copy(value)
 
   def debug(self):
     if(self.node_left != None):
@@ -28,15 +31,13 @@ class Node():
       self.node_right.debug()
 
 
-
-
 class Tree():
   def __init__(self, max_depth):
     self.root = Node(None, None, None)
-    self.max_depth = max_depth
+    self.max_depth = copy.copy(max_depth)
 
   def set_root(self, root):
-    self.root = root
+    self.root = copy.copy(root)
 
   def debug(self):
     self.root.debug()
@@ -46,34 +47,46 @@ class Tree():
 #Y: list of labels data 1D numpy array
 #max_depth is the max depth for the resulting tree
 def DT_train_binary(X,Y, max_depth):
+  for features in range(X.shape[1]):
+    feature_dict.update({str(features): 0})
+
+  for x , y in feature_dict.items():
+    print(x, y)
+
   print("Features: \n", X, "\n")
   print("Labels: \n", Y, "\n")
-  samples = X.shape[0]
-  features = X.shape[1]
-  labels = Y.shape[0]
-
-
+  #samples = X.shape[0]
+  #features = X.shape[1]
+  #labels = Y.shape[0]
   print(entropy_tree(Y))
-
-  DT_tree = Tree(max_depth)
-  DT_tree.debug()
-
-
+  entropy_subtree(X, Y, max_depth)
+  #DT_tree = Tree(max_depth)
+  #DT_tree.debug()
 
 def entropy_tree(tree):
   num_false = 0
   num_true = 0
   total_features = tree.shape[0]
   for x in range(tree.shape[0]):
-    print(tree[x][0])
+    #print(tree[x][0])
     if(tree[x][0] == 1):
       num_true = num_true + 1
     elif(tree[x][0] == 0):
       num_false = num_false + 1
-  print("num_true: ", num_true)
-  print("num_false: ", num_false)
-
+  #print("num_true: ", num_true)
+  #print("num_false: ", num_false)
   return(calc_entry(num_false, num_true, total_features))
+
+
+def entropy_subtree(features, labels, max_depth):
+  if(max_depth == -1):
+    return entropy_subtree(features, labels, max_depth)
+  elif(max_depth == 0):
+    return
+  else:
+    print(max_depth)
+
+    return entropy_subtree(features, labels, max_depth - 1)
 
 
 def calc_entry(n, y, total):
