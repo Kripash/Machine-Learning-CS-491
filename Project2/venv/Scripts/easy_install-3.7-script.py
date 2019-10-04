b4223a9f73c4bb76d2b690af1427810834ce3dcf
torben-w <towalang@web.de>
2019-10-04 10:46:55 -0700

    set up python project

---
+++ Project2/venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!"X:\Google Drive\Studium\7. Semester WS19-20\CS 491 ML\projects\project2\Machine-Learning-CS-491\Project2\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
---
+++ Project2/venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!"X:\Google Drive\Studium\7. Semester WS19-20\CS 491 ML\projects\project2\Machine-Learning-CS-491\Project2\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip')()
+    )
