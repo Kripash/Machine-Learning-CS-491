cf7ff0fda9454592a51b1250d4c82378b7bc25bb
torben-w <towalang@web.de>
2019-10-11 22:39:50 -0700

    finished better_kmains(X,K) - still needs testing - there is potential for improvement in find_majority()

--- Project2/clustering.py
+++ Project2/clustering.py
@@ -151,3 +151,66 @@ def K_Means(X, K):
 
     # return cluster_centers as numpy array
     return np.array(cluster_centers)
+
+
+def find_majority(all_cluster_centers, index):
+    majority_cluster_centers = []
+    number_of_kmeans_computations = index
+    max_occurrences = 0
+    number_of_results_for_max_occurrences = 0  # There are no majority cluster_centers if there are multiple different solutions
+
+    for centers, amount in all_cluster_centers.items():
+        if not majority_cluster_centers:
+            majority_cluster_centers = centers
+            max_occurrences = amount
+            number_of_results_for_max_occurrences = 1
+        else:
+            if amount > max_occurrences:
+                majority_cluster_centers = centers
+                max_occurrences = amount
+                number_of_results_for_max_occurrences = 1
+            elif amount == max_occurrences:
+                majority_cluster_centers = []  # There is no majority at this moment
+                number_of_results_for_max_occurrences += 1
+
+    proportion = max_occurrences / number_of_kmeans_computations
+
+    # TODO Create some threshold "proportion". We don't really have a majority if there are multiple solutions with
+    # TODO almost similar occurrences.
+
+    return majority_cluster_centers
+
+
+def K_Means_better(X,K):
+    majority_cluster_centers = []
+    all_cluster_centers = {}
+
+    there_is_a_majority = False
+    index = 1
+    max_iterations = 10000  # we don't want to wait for hours...
+    min_iterations = 100  # but we should still try kmeans many times
+    while True:
+        cluster_centers = K_Means(X, K)
+
+        # Order cluster_centers by first value (x0), then x1, then x2, ... in order to get a distinct key for the
+        # dictionary all_cluster_centers
+        cluster_centers = sorted(cluster_centers)
+
+        # Save the number of times a cluster center is the result of K_Means(X,Y)
+        if cluster_centers not in all_cluster_centers:
+            all_cluster_centers[cluster_centers] = 1
+        else:
+            all_cluster_centers[cluster_centers] += 1
+
+        # find out if there is a majority
+        majority_cluster_centers = find_majority(all_cluster_centers, index)
+        if majority_cluster_centers is not None and majority_cluster_centers:
+            there_is_a_majority = True
+
+        if (there_is_a_majority and index >= min_iterations) or index == max_iterations:
+            break
+
+        index += 1
+
+    # return majority_cluster_centers as numpy array
+    return np.array(majority_cluster_centers)
