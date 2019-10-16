6a1fbddbdc305f8a3783417d8b483eed834b8534
Kripash Shrestha <kripashs@gmail.com>
2019-10-15 21:48:11 -0700

    Added comments
    comments for perceptron and nearest_neighbors

--- Project2/nearest_neighbors.py
+++ Project2/nearest_neighbors.py
@@ -2,6 +2,14 @@ import copy
 import numpy as np
 import math
 
+
+"""
+KNN point object:
+feature: feature index of the object
+label: label of the point
+classify: classification of the point (for prediction)
+euclid: euclid distance of point to current test
+"""
 class point:
   def __init__(self, feature, label, euclid, classify):
     self.feature = copy.copy(feature)
@@ -18,10 +26,33 @@ class point:
   def getEuclid(self):
     return self.euclid
 
+
+"""
+euclidian_distance(x,y):
+  calculate the euclidian using (y - x)^2 
+  we square root the entire sum later on
+"""
 def euclidian_distance(x, y):
   return ((y - x)**2)
 
-
+"""
+KNN_test(X_train, Y_train, X_test, Y_test, K):
+  The initial loop will:
+  iterate through the number of testing data
+  For each testing data, iterate through the training data 
+  and for each feature, calculate the euclidian distance 
+  
+  After that has been done, each testing data will then have the 
+  data sorted in ascending order based on the euclidian distance and append it to test points 
+  
+  The function will loop through the test points and based on the K value, it will go from the ordered list 
+  based on euclidian distance up till the Kth element and calculate the prediciton for that test label 
+  and append it to the prediction list. 
+  
+  After the prediction step is complete, the function will go through the number of x_test and check to see 
+  if the sign of the prediction matches the sign of the label for that data set and calculate the number of correct
+  predictions. The function will then return the accuracy as a prediction.
+"""
 def KNN_test(X_train, Y_train, X_test, Y_test, K):
   #print(K)
   test_points = []
@@ -57,6 +88,15 @@ def KNN_test(X_train, Y_train, X_test, Y_test, K):
 
 
 
+"""
+choose_K(X_train, Y_train, X_val, Y_val):
+  Set a tuple to be acurracy of -1, and K value of -1 
+  go in a for loop within the range of the Y_training data set
+  call KNN_test and increment K in the range of Y_training data set 
+  if the accurracy is greater than the current stored in the tuple, 
+  store that accuracy as tuple object 1 and the K as tuple object 2
+  return the tuple object 2 for k value
+"""
 def choose_K(X_train, Y_train, X_val, Y_val):
 
   best_training = (-1, -1)
--- Project2/perceptron.py
+++ Project2/perceptron.py
@@ -2,13 +2,21 @@ import numpy as np
 import math
 import copy
 
-X = np.array([[0,1] , [1,0] , [5,4] , [1,1] , [3,3] , [2,4] , [1,6] ])
-Y = np.array([[1], [1], [0], [1], [0], [0], [0]])
+#X = np.array([[0,1] , [1,0] , [5,4] , [1,1] , [3,3] , [2,4] , [1,6] ])
+#Y = np.array([[1], [1], [0], [1], [0], [0], [0]])
 
 #X = np.array([[-2,1] , [1,1] , [1.5,-0.5] , [-2,-1] , [-1,-1.5] , [2,-2] ])
 #Y = np.array([[1], [1], [1], [-1], [-1], [-1]])
 
-
+"""
+Perceptron object:
+bias: bias of the perceptron 
+weights: weights of the perceptron 
+data: the features of data set
+labels: labels of data set
+epoch_variable: number of epochs
+last_weight_change: track which was the last weight change
+"""
 class perceptron():
   def __init__(self, X, Y):
     self.bias = 0
@@ -28,6 +36,21 @@ class perceptron():
     print("")
 
 
+"""
+perceptron_train:
+  The function takes in some data set X with features and some Y with labels. 
+  The function goes in an infinite loop. 
+  In the loop:
+  The function will keep track of the last_weight_change and if it does 1 complete loop, the function will 
+  return, because it has completed 1 full epoch without changing weights. 
+  The function will perform the perceptron algorith. For this, we assume a data set of 0 is also -1 since 
+  that is what the original data set consisted of. The function will calculate a, such that 
+  a = W * X + bias 
+  if a * Y <= 0: 
+  update perceptron so that:
+  W = W + X * Y 
+  bias = bias + Y
+"""
 def perceptron_train(X, Y):
   perceptron_local = perceptron(X, Y)
   #perceptron_local.debug()
@@ -67,6 +90,14 @@ def perceptron_train(X, Y):
 
     #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
+"""
+perceptron_test:
+  The function will take in some X features of test set and Y labels of test data and the weights and bias. 
+  The function will go through the number of labels and calculate the prediction using: 
+  prediction = W * X + b
+  If the sign of the prediction is equal to the label, the function increments the number of correct predictions by 1. 
+  The function will return the accuracy based on num of correct predictions over the total test set.
+"""
 
 def perceptron_test(X_test, Y_test, w, b):
   num_correct = 0
@@ -85,8 +116,8 @@ def perceptron_test(X_test, Y_test, w, b):
 
 
 
-W = perceptron_train(X, Y)
+#W = perceptron_train(X, Y)
 
-accuracy = perceptron_test(X, Y, W[0], W[1])
+#accuracy = perceptron_test(X, Y, W[0], W[1])
 
 #print(accuracy)
