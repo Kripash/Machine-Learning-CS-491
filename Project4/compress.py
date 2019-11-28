1320eba450c0dab0f5c3288f499ac21bb9a049af
satchelh <golfprohilo02@gmail.com>
2019-11-28 01:24:22 -0800

    Project 4 image compression finally working!

--- Project4/compress.py
+++ Project4/compress.py
@@ -8,7 +8,7 @@ def load_data(input_dir):
     if os.path.exists(input_dir):
         flattened_images = []
         for image in os.listdir(input_dir):
-            flattened_images.append( plt.imread(input_dir + '/' + image).flatten('F') )
+            flattened_images.append( plt.imread(input_dir + '/' + image).flatten() )
 
         flattened_images = np.transpose(flattened_images)
         flattened_images = flattened_images.astype(float) ## convert all values in 
@@ -25,17 +25,13 @@ def compress_images(DATA, k):
     # Get the first k principal components:
 
     U_T = PCS.transpose()[:k]
-     ## Problem starts here, we need only k pc's
-    #print('\n\n\n\n\n\n\n\n\n\n\n\n')
-    # At this point, PCS is of size 2880x2880 since there are 2880 'features' (our dimension)
-    # and so it is assumed that there will be 2880 eigenvalues, and hence 2880 respective 
-    # eigenvectors. So, U_T is also 2880 x 2880
+    # At this point, U_TS is of size 2880 x k since there are 2880 'features' (our dimension)
     compressed_images = np.dot(Z_star, U_T)
     # continue from here, just need to output images into directory OUTPUT and 
     # use imsave with cmap = 'gray' option to save as grayscale.
     for x in range(len(DATA)):
         output = "/home/satchelh/Desktop/CS491/test_output/image" + str(x) + ".png"
-        plt.imsave(output, compressed_images[x].reshape((60, 48), order='F'),cmap='gray')
+        plt.imsave(output, compressed_images[x].reshape(60, 48), cmap='gray')
 
 DATA = load_data("/home/satchelh/Desktop/CS491/Project4-2/Project4/Data/Train")
 compress_images(DATA, 100)
--- Project4/pca.py
+++ Project4/pca.py
@@ -2,59 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
-debug = False
 
 def compute_Z(X, centering=True, scaling=False):
-  global debug
   Z = copy.copy(X)
   mean = np.mean(Z, axis=0)
   std = np.std(Z, axis=0)
-  if debug:
-    print("Mean: ", mean)
-    print("Std: ", std)
+
   if(centering):
     Z = Z - mean
-  if debug:
-    print("Z: \n", Z)
+ 
   if(scaling):
     Z = Z/std
-  if debug:
-    print("Scaling: \n", Z)
 
   return Z
 
 
 def compute_covariance_matrix(Z):
-  global debug
   Z_T = np.transpose(Z)
   covariance_z = np.dot(Z_T, Z)
-  if debug:
-    print("covariance_z: \n", covariance_z)
+
   return covariance_z
 
 
 def find_pcs(COV):
-  global debug
   L, PCS = np.linalg.eig(COV)
   L = L.real 
   PCS = PCS.real
-  if debug:
-    print("eigen values: \n", L)
-    print("eigen vectors: \n", PCS)
 
-  indices = L[::-1].argsort()
-  L[::-1].sort()
-  if debug:
-    print("Sorted Eigen Values: \n", L)
-    print("Sorted Eigen Vectors: \n", PCS)
-    print("Indices: \n", indices)
+  indices = np.flip( L.argsort() ) # indices now has indices of eigenvalues in the order of largest to smallest.
+  print(indices)
+  L = np.flip( L.sort() )
 
-  PCS = PCS.T[indices].T
-  if debug:
-    print("PCS: \n", PCS)
+  PCS = PCS[:, indices]
 
   return L, PCS
-
 
 def determineK(L, var):
   sum = np.sum(L)
@@ -68,24 +49,16 @@ def project_data(Z, PCS, L, k, var):
   if(k == 0):
     k = determineK(L, var)
 
-  if debug:
-    print("L: ", k)
   U = PCS[:, :k]
-  if debug:
-    print("U: \n", U)
-    print("Z: \n", Z)
-  projection = np.dot(Z, U)
 
-  if debug:
-    print("projection: \n", projection)
+  projection = np.dot(Z, U)
 
   return projection
 
 
-
-"""X = np.array([[1, -1], [-1, 1], [1, -1], [1,1]])
-Z = compute_Z(X, centering=True, scaling=True)
-cov_z = compute_covariance_matrix(Z)
-L, PCS = find_pcs(cov_z)
-print(PCS)
-Z_star = project_data(Z, PCS, L, 1, 0)"""
+# X = np.array([[1, -1], [-1, 1], [1, -1], [1,1]])
+# Z = compute_Z(X, centering=True, scaling=True)
+# cov_z = compute_covariance_matrix(Z)
+# L, PCS = find_pcs(cov_z)
+# print(PCS)
+# Z_star = project_data(Z, PCS, L, 1, 0)
