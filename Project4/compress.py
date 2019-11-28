7e5d98b2eb8caaf3e3ba839e437bafddb83f3ef9
satchelh <golfprohilo02@gmail.com>
2019-11-27 19:17:31 -0800

    Update both compress.py and pca.py

--- Project4/compress.py
+++ Project4/compress.py
@@ -2,12 +2,13 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pca
+import copy
 
 def load_data(input_dir):
     if os.path.exists(input_dir):
         flattened_images = []
         for image in os.listdir(input_dir):
-            flattened_images.append( plt.imread(input_dir + '/' + image).flatten() )
+            flattened_images.append( plt.imread(input_dir + '/' + image).flatten('F') )
 
         flattened_images = np.transpose(flattened_images)
         flattened_images = flattened_images.astype(float) ## convert all values in 
@@ -17,19 +18,24 @@ def load_data(input_dir):
 def compress_images(DATA, k):
     DATA = np.transpose(DATA)
 
-    Z = pca.compute_Z(DATA, centering=True, scaling=True)
+    Z = pca.compute_Z(DATA)
     cov_z = pca.compute_covariance_matrix(Z)
     L, PCS = pca.find_pcs(cov_z)
     Z_star = pca.project_data(Z, PCS, L, k, 0)
-    U_T = np.transpose(PCS) ## Problem starts here, we need only k pc's
-    print('\n\n\n\n\n\n\n\n\n\n\n\n')
+    # Get the first k principal components:
+
+    U_T = PCS.transpose()[:k]
+     ## Problem starts here, we need only k pc's
+    #print('\n\n\n\n\n\n\n\n\n\n\n\n')
     # At this point, PCS is of size 2880x2880 since there are 2880 'features' (our dimension)
     # and so it is assumed that there will be 2880 eigenvalues, and hence 2880 respective 
     # eigenvectors. So, U_T is also 2880 x 2880
-    compressed_images = np.dot(np.transpose(Z_star), U_T)
+    compressed_images = np.dot(Z_star, U_T)
     # continue from here, just need to output images into directory OUTPUT and 
     # use imsave with cmap = 'gray' option to save as grayscale.
+    for x in range(len(DATA)):
+        output = "/home/satchelh/Desktop/CS491/test_output/image" + str(x) + ".png"
+        plt.imsave(output, compressed_images[x].reshape((60, 48), order='F'),cmap='gray')
 
-
-DATA = load_data("/home/satchel/Downloads/Project4-2/Project4/Data/Train")
-compress_images(DATA, 1)
+DATA = load_data("/home/satchelh/Desktop/CS491/Project4-2/Project4/Data/Train")
+compress_images(DATA, 100)
--- Project4/pca.py
+++ Project4/pca.py
@@ -27,7 +27,7 @@ def compute_Z(X, centering=True, scaling=False):
 def compute_covariance_matrix(Z):
   global debug
   Z_T = np.transpose(Z)
-  covariance_z = np.matmul(Z_T, Z)
+  covariance_z = np.dot(Z_T, Z)
   if debug:
     print("covariance_z: \n", covariance_z)
   return covariance_z
@@ -36,6 +36,8 @@ def compute_covariance_matrix(Z):
 def find_pcs(COV):
   global debug
   L, PCS = np.linalg.eig(COV)
+  L = L.real 
+  PCS = PCS.real
   if debug:
     print("eigen values: \n", L)
     print("eigen vectors: \n", PCS)
@@ -43,10 +45,11 @@ def find_pcs(COV):
   indices = L[::-1].argsort()
   L[::-1].sort()
   if debug:
-    print("Sorted Eigen Vectors: \n", L)
+    print("Sorted Eigen Values: \n", L)
+    print("Sorted Eigen Vectors: \n", PCS)
     print("Indices: \n", indices)
 
-  PCS = PCS[:, indices]
+  PCS = PCS.T[indices].T
   if debug:
     print("PCS: \n", PCS)
 
@@ -58,22 +61,20 @@ def determineK(L, var):
   for i in range(len(L)):
     variance = np.sum(L[:i]) / sum
     if(variance >= var):
-      return i
+      return i 
   return len(L)
 
-
 def project_data(Z, PCS, L, k, var):
-  local_k = copy.copy(k)
-  if(local_k == 0):
-    local_k = determineK(L, var)
+  if(k == 0):
+    k = determineK(L, var)
 
   if debug:
-    print("L: ", local_k)
-  U = PCS.transpose()[:local_k].transpose()
+    print("L: ", k)
+  U = PCS[:, :k]
   if debug:
     print("U: \n", U)
     print("Z: \n", Z)
-  projection = np.matmul(Z, U)
+  projection = np.dot(Z, U)
 
   if debug:
     print("projection: \n", projection)
@@ -82,9 +83,9 @@ def project_data(Z, PCS, L, k, var):
 
 
 
-X = np.array([[-1, -1], [-1, 1], [1, -1], [1,1]])
+"""X = np.array([[1, -1], [-1, 1], [1, -1], [1,1]])
 Z = compute_Z(X, centering=True, scaling=True)
 cov_z = compute_covariance_matrix(Z)
 L, PCS = find_pcs(cov_z)
 print(PCS)
-Z_star = project_data(Z, PCS, L, 1, 0)
+Z_star = project_data(Z, PCS, L, 1, 0)"""
